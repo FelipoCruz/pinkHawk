c1e6adbb177c9b90094516b96d827e9922ef3fd6
dr3nan <dolgarrenan@gmail.com>
2023-01-28 16:59:20 +0100

    chore: move slices folder into store folder, refs #500

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from '../slices/auth.slice';
-import userReducer from '../slices/user.slice';
+import authReducer from './slices/auth.slice';
+import userReducer from './slices/user.slice';
 
 export const store = configureStore({
   reducer: {
