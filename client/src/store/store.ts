67879ec28ada481e41dd27958122c84459bed5fa
dr3nan <dolgarrenan@gmail.com>
2023-01-28 16:47:45 +0100

    feat: add redicers to store, refs #500

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,5 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import authReducer from '../slices/auth.slice';
+import userReducer from '../slices/user.slice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: authReducer,
+    user: userReducer,
+  },
 });
