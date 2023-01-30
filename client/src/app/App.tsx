13180b7186b430ad89fbe9770c198818dc74f860
Jaafar Fora <jaafarfora@gmail.com>
2023-01-30 15:22:37 +0100

    fix: change layout - getUserById

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { getUserById } from '../services/api.service';
+import { activeUser } from '../store/slices/user.slice';
 import './App.scss';
 import Header from './components/header/Header';
 import Login from './components/login/SignIn/login';
 import SignUp from './components/login/SignUp/sign-up';
-import { useAppSelector } from './hooks/hooks';
+import { useAppDispatch, useAppSelector } from './hooks/hooks';
 import Dashboard from './routes/dashboard';
 import HomePage from './routes/home-page';
 import ProtectedRoute from './routes/protected-route';
 
 const App: React.FC = (): JSX.Element => {
   const loggeIn = useAppSelector((state) => state.auth.isLoggedIn);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (!loggeIn) {
+      (async () => {
+        let storedString = localStorage.getItem('user');
+        if (storedString) {
+          const storedUser = JSON.parse(storedString);
+
+          if (storedUser) {
+            const user = await getUserById(storedUser.id);
+            console.log(user);
+
+            dispatch(activeUser(user));
+          }
+        }
+      })();
+    }
+  }, []);
 
   return (
     <>
-      <Header />
       <div className="container">
         <Routes>
-          <Route
-            path="/"
-            element={loggeIn ? <Navigate to="/dashboard" /> : <HomePage />}
-          />
+          <Route path="/" element={<HomePage />} />
           <Route
             path="/dashboard"
             element={
