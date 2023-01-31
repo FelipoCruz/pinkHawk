54a8082b77433cc0420f33a03f4835792db7af36
dr3nan <dolgarrenan@gmail.com>
2023-01-31 19:51:09 +0100

    feat: add functionality to filter by status (WIP), refs #503

--- client/src/app/components/navbar/copilotmenubutton/Queu/Queue.tsx
+++ client/src/app/components/navbar/copilotmenubutton/Queu/Queue.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
 import { useAppDispatch, useAppSelector } from '../../../../hooks/hooks';
+import Tweet from '../../../tweet/Tweet';
 
 const Queue = () => {
   const dispatch = useAppDispatch();
   const user = useAppSelector(({ user }) => user);
   console.log('user from state', user);
-  const tweet = useAppSelector(({ tweets }) => tweets);
-  console.log('tweets from state', tweet);
+  const tweets = useAppSelector(({ tweets }) => tweets);
+  console.log('tweets from state', tweets);
+
 
   return (
     <div>
-      <h1>Queue of tweets</h1>
-      {tweet.tweets.map((tweet: any) => {
-        return (
-          <div key={tweet.id}>
-            <p>{tweet}</p>
-          </div>
-        );
-      })}
-
+      <ul>
+        {tweets.tweets.filter((tweet: any) => tweet.status === 'queue').map((tweet: any) => {
+          return <Tweet key={tweet.id} tweet={tweet} />;
+        })}
+      </ul>
     </div>
   );
 };
