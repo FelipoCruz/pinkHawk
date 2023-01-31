33d9578a53771ed508cd7cb6135177c999366c03
dr3nan <dolgarrenan@gmail.com>
2023-01-31 18:34:43 +0100

    feat: initial commit on 503 (WIP), refs #503

--- client/src/app/components/navbar/copilotmenubutton/Queu/Queue.tsx
+++ client/src/app/components/navbar/copilotmenubutton/Queu/Queue.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useAppDispatch, useAppSelector } from '../../../../hooks/hooks';
 
 const Queue = () => {
-  
+  const dispatch = useAppDispatch();
+  const user = useAppSelector(({ user }) => user);
+  console.log('user from state', user);
+  const tweet = useAppSelector(({ tweets }) => tweets);
+  console.log('tweets from state', tweet);
+
   return (
     <div>
+      <h1>Queue of tweets</h1>
+      {tweet.tweets.map((tweet: any) => {
+        return (
+          <div key={tweet.id}>
+            <p>{tweet}</p>
+          </div>
+        );
+      })}
 
     </div>
   );
