ab38d1c1f112d54ec7b39f5f8c4f9f10e5857584
dr3nan <dolgarrenan@gmail.com>
2023-01-31 20:28:58 +0100

    feat: continuos work on queue component (WIP), refs #503

--- client/src/app/components/navbar/copilotmenubutton/Queu/Queue.tsx
+++ client/src/app/components/navbar/copilotmenubutton/Queu/Queue.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { useAppDispatch, useAppSelector } from '../../../../hooks/hooks';
 import Tweet from '../../../tweet/Tweet';
+import { Tweet as TweetType } from '../../../../../store/slices/tweet.slice';
 
 const Queue = () => {
   const dispatch = useAppDispatch();
   const user = useAppSelector(({ user }) => user);
   console.log('user from state', user);
-  const tweets = useAppSelector(({ tweets }) => tweets);
+  const { tweets } = useAppSelector(({ tweets }) => tweets);
   console.log('tweets from state', tweets);
 
 
   return (
     <div>
       <ul>
-        {tweets.tweets.filter((tweet: any) => tweet.status === 'queue').map((tweet: any) => {
-          return <Tweet key={tweet.id} tweet={tweet} />;
+        {tweets?.length > 0 && tweets.filter((tweet: TweetType) => tweet.status === 'queue').map((tweet: TweetType) => {
+          return <li><Tweet key={tweet.id} tweetPassed={tweet} /></li>;
         })}
       </ul>
     </div>
