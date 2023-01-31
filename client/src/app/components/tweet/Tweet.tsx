0de41fc196fcb56a6f98052960bc3783efe34d9d
dr3nan <dolgarrenan@gmail.com>
2023-01-31 15:59:55 +0100

    feat: tweet component (WIP), refs #502

--- client/src/app/components/tweet/Tweet.tsx
+++ client/src/app/components/tweet/Tweet.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { deleteTweet, Tweet, TweetState } from '../../../store/slices/tweet.slice';
+import { deleteTweet, Tweet } from '../../../store/slices/tweet.slice';
 import { activeUser } from '../../../store/slices/user.slice';
 import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
 import { getUserById } from '../../../services/api.service';
 import { queueTweets } from '../../../store/slices/tweet.slice';
 import { UserState } from '../../../store/slices/user.slice';
 
-const Tweet: React.FC = ({ tweetPassed }) => {
+const SingleTweet: React.FC = ({ tweetPassed }) => {
   const dispatch = useAppDispatch();
   const user = useAppSelector(({ user }) => user);
 
@@ -18,7 +18,7 @@ const Tweet: React.FC = ({ tweetPassed }) => {
       // get user with new tweets from DB
       const getUserFromAPI = await getUserById(user.id);
       // update tweet state
-      dispatch(deleteTweet(deleteTweet));
+      dispatch(deleteTweet(deleteSingleTweet));
       // update user state with fetched user
       dispatch(activeUser(getUserFromAPI));
     } catch (err) {
@@ -29,7 +29,7 @@ const Tweet: React.FC = ({ tweetPassed }) => {
   const handleSelected = async (user: UserState, tweet: Tweet) => {
     try {
       // update tweet status in DB, API to be made
-      const tweetWithNewStatus = await changeTweetStatusFromUserAPI(user.id, (tweet.status = 'queue'));
+      const tweetWithNewStatus = await changeTweetStatusFromUserAPI(user.id, tweet.id, 'queue');
       console.log('file: tweet.tsx:32 ~~> handleSelected ~~> tweetWithNewStatus', tweetWithNewStatus)
       // get user with new tweets from DB
       const getUserFromAPI = await getUserById(user.id);
@@ -40,13 +40,22 @@ const Tweet: React.FC = ({ tweetPassed }) => {
     } catch (err) {
       console.error(err);
     }
-  }
+  };
 
   return (
-    <div>
-
+    <div className='solo-tweet'>
+      <div className='user-details'>
+        <img src={tweetPassed.user.profilePic} alt='profile-pic' />
+        <div className='user-info'>
+          <h3>{tweetPassed.user.name}</h3>
+          <p>{tweetPassed.user.username}</p>
+        </div>
+      </div>
+      <div className='tweet-text'>
+        <p>{tweetPassed.text}</p>
+      </div>
     </div>
   );
 };
 
-export default Tweet;
+export default SingleTweet;
--- client/src/store/slices/tweet.slice.ts
+++ client/src/store/slices/tweet.slice.ts
@@ -1,9 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 export interface Tweet {
-  id: string;
+  id: number;
   status: string;
-  content: string;
+  text: string;
 }
 
 export interface TweetState {
