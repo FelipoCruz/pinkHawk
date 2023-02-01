f157106ae683257dff901f93d768c1b927c6e0d2
IsNotSally <verena0915@gmail.com>
2023-02-01 17:46:44 +0100

    styling the topics-input, add lastpost time to user model

--- client/src/app/components/topics-input/topics-input.tsx
+++ client/src/app/components/topics-input/topics-input.tsx
@@ -8,14 +8,15 @@ import "./topics-input.scss"
 const TopicsInput = () => {
   const user = useAppSelector((state) => state.user)
   console.log('user id us :', user)
-  const [selectedTopics, setSelectedTopics] = useState([""]);
+  const [selectedTopics, setSelectedTopics] = useState<string[]>([]);
   const [timePreference, setTimePreference] = useState('')
 
   const setTopics = () => {
     console.log('selected topics are:', selectedTopics)
     saveTopics(selectedTopics, user.email)
+    setSelectedTopics([])
   }
-  
+
 
   const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setTimePreference(e.target.value)
@@ -24,43 +25,48 @@ const TopicsInput = () => {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
     const pref = await updateTimePreference(user.id, timePreference)
+    setTimePreference('')
   }
-  
-  return (
-    <div className='topics-input-container'>
-      <h1>It´s time to define what you want to tweet about.</h1>
-      <h2>Write 20 tags, that define the most important topics you want to tweet about:</h2>
-      {/* <pre>{JSON.stringify(selected)}</pre> */}
-      <TagsInput
-        value={selectedTopics}
-        onChange={setSelectedTopics}
-        name="fruits"
-        placeHolder="enter here the topics you want to tweet about"
-      />
-      <em>press enter or comma to add new tag</em>
-      <button onClick={setTopics}>submit topics</button>
 
-      <form onSubmit={handleSubmit}>
-        <h2 className='time-label'>Choose your preference for posting a tweet:</h2>
-        <label htmlFor="number">every</label>
-        <select id="number" name="number" className='select-box'  onChange={handleChange} value={timePreference}>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-          <option value="10">10</option>
-          <option value="11">11</option>
-          <option value="12">12</option>
-        </select>
-        <label>hour(s)</label>
-        <button className='pref-btn'>Save the preference</button>
-      </form>
-      
+  return (
+    <div className='pref-container'>
+
+      <div className='topics-input-container'>
+        <h1>Tweets tags</h1>
+        <p>Define the most important topics you want to tweet about:</p>
+        <TagsInput
+          value={selectedTopics}
+          onChange={setSelectedTopics}
+          name="tags"
+          placeHolder="Enter here tweet tags"
+        />
+        <em>(Press enter to add new tag)</em>
+        <button onClick={setTopics} className='pref-btn'>submit topics</button>
+      </div>
+
+      <div className='form-container'>
+        <form onSubmit={handleSubmit}>
+          <h1 className='time-label'>Choose your preference for posting a tweet:</h1>
+          <label htmlFor="number">every</label>
+          <select id="number" name="number" className='select-box' onChange={handleChange} value={timePreference}>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+            <option value="11">11</option>
+            <option value="12">12</option>
+          </select>
+          <label>hour(s)</label>
+          <button className='pref-btn'>Save preference</button>
+        </form>
+      </div>
+
     </div>
   );
 };
