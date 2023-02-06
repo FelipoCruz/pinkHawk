a66e32c6b010df562fd0dc76b8b745d0458641ff
Phil Cruz <mfel.cruz@gmail.com>
2023-02-06 18:03:18 +0100

    trying to deploy

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,7 +7,6 @@ import cookieParser from 'cookie-parser';
 import { job, job1 } from './integration/twitter-service/twitter-api.service';
 
 dotenv.config({ path: path.join(__dirname, '../.env') });
-const CLIENT_URL = process.env.REACT_APP_CLIENT_URL;
 const port = process.env.SERVER_PORT || 5000;
 const app = express();
 
