e88104a6917e394de81510829c30367dc4d450a7
Jaafar Fora <jaafarfora@gmail.com>
2023-02-08 13:27:25 +0100

    Feat: handle no pref

--- client/src/app/components/loginnavbar/LoginNavBar.tsx
+++ client/src/app/components/loginnavbar/LoginNavBar.tsx
@@ -1,14 +1,32 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import './LoginNavBar.scss';
 import { ReactComponent as Logo } from '../../../images/pinkhawklogo.svg';
 import { toggleSidebar } from '../../../store/slices/layout.slice';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { CgProfile } from 'react-icons/cg';
 import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
+import { IoWarningOutline } from 'react-icons/io5';
+import Tooltip from '../tooltip/tooltip';
+import { logout } from '../../../services/api.service';
+import { deactivateUser } from '../../../store/slices/user.slice';
 
 const NavBarUser = () => {
+  const user = useAppSelector((state) => state.user);
   const layout = useAppSelector((state) => state.layout);
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const logoutUser = async () => {
+    const response = await logout();
+
+    if (response.status !== 'success') {
+      throw new Error('A problem occurred while logging out');
+    }
+
+    dispatch(deactivateUser());
+    localStorage.removeItem('user');
+    navigate('/dashboard');
+  };
 
   const toggleClick = () => {
     dispatch(toggleSidebar(!layout.sidebar));
@@ -16,29 +34,29 @@ const NavBarUser = () => {
 
   return (
     <header className="user-navbar fixed-top">
-      {/* <button className="mobile-menu-button" onClick={toggleClick}> */}
       <GiHamburgerMenu className="mobile-menu-button" onClick={toggleClick} />
-      {/* </button> */}
       <a href="/dashboard" className="logo-container">
         <Logo className="logo" />
         <span className="logo-text">Pink Hawk</span>
       </a>
-      {/* <nav className="navbar-items">
-        <NavLink to="/dashboard/co-pilot">
-          <img
-            alt="copilot-button"
-            className="icon-button"
-            src={copilotButton}
-          />
-        </NavLink>
-        <NavLink to="/dashboard/growth">
-          <img alt="growth-button" className="icon-button" src={growthButton} />
-        </NavLink>
-      </nav> */}
       <nav className="navbar-items">
-        <NavLink to="user-settings">
-          <CgProfile />
-        </NavLink>
+        <div className="navbar-warning">
+          {(!user.twitterToken || user.frequencyTweetPosting < 1) && (
+            <Tooltip
+              content1="- Add your preferences !"
+              content2="- Give access to your twitter account !"
+              direction="bottom"
+              type="warning"
+            >
+              <IoWarningOutline />
+            </Tooltip>
+          )}
+        </div>
+        <div className="navbar-user">
+          <Tooltip direction="bottom" type="user-menu" logout={logoutUser}>
+            <CgProfile />
+          </Tooltip>
+        </div>
       </nav>
     </header>
   );
--- client/src/app/components/userpreferences/UserPreferences.tsx
+++ client/src/app/components/userpreferences/UserPreferences.tsx
@@ -26,31 +26,24 @@ const UserPreferences = () => {
     }
   };
 
-  const logoutUser = async () => {
-    const response = await logout();
-
-    if (response.status !== 'success') {
-      throw new Error('A problem occurred while logging out');
-    };
-
-    dispatch(deactivateUser());
-    localStorage.removeItem('user');
-    navigate('/dashboard');
-  };
-
   return (
     <>
-      <div className='container-user-settings'>
+      <div className="container-user-settings">
         <h1>User Preferences</h1>
-        <div className='right-menu-slide-right'>
+        <div className="right-menu-slide-right">
           <RightMenuButton />
         </div>
+        <div className="current-user-settings" onClick={handleClickNavigate}>
+          <div className="frequency-tweet-posting">
+            <p>Current daily posting frequency:</p>
         <div className='current-user-settings' onClick={handleClickNavigate}>
         <label className='current-preferences-user' htmlFor='user-name'>Current Preferences</label>
           <div className='frequency-tweet-posting'>
             <p>Daily posting frequency:</p>
             {user.frequencyTweetPosting}
           </div>
+          <div className="selected-hours">
+            <p>Selected posting hours:</p>
           <div className='selected-hours'>
             <p>Posting hours:</p>
             {user.postingHours.map((hour: number) => (
@@ -58,9 +51,14 @@ const UserPreferences = () => {
             ))}
           </div>
         </div>
+        <div className="connection-to-twitter">
         <div className='connection-to-twitter'>
           <div className='connection-status'>
           <p>Connection to Twitter:</p>
+          {user.twitterToken !== null
+            ? 'Connected to Twitter'
+            : 'Not connected to Twitter'}
+          <button onClick={handleClick} className="connect-btn">
             {user.twitterToken !== null
               ? 'Connected!'
               : 'Not connected'}
@@ -80,4 +78,3 @@ const UserPreferences = () => {
 };
 
 export default UserPreferences;
-
