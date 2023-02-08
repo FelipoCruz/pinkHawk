11a08942a5ae37520f91113db357c55566d8d596
dr3nan <dolgarrenan@gmail.com>
2023-02-08 15:51:21 +0100

    fix: user preferences now work on different sizes, refs #508

--- client/src/app/components/userpreferences/UserPreferences.tsx
+++ client/src/app/components/userpreferences/UserPreferences.tsx
@@ -1,13 +1,12 @@
 // import { useState } from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
-import { getAuthUrl, logout } from '../../../services/api.service';
-import { deactivateUser } from '../../../store/slices/user.slice';
+import { useNavigate } from 'react-router-dom';
+import { getAuthUrl } from '../../../services/api.service';
 import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
 import RightMenuButton from './user-submenu/UserSubmenu';
 import './UserPreferences.scss';
 
 const UserPreferences = () => {
-  const dispatch = useAppDispatch();
+  // const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const user = useAppSelector(({ user }) => user);
   // TODO: make the connect to twitter button appear only when not connected
@@ -28,32 +27,32 @@ const UserPreferences = () => {
 
   return (
     <>
-      <div className="container-user-settings">
+      <div className='container-user-settings'>
         <h1>User Preferences</h1>
-        <div className="right-menu-slide-right">
+        <div className='right-menu-slide-right'>
           <RightMenuButton />
         </div>
-        <div className="current-user-settings" onClick={handleClickNavigate}>
-          <label className="current-preferences-user" htmlFor="user-name">
+        <div className='current-user-settings' onClick={handleClickNavigate}>
+          <label className='current-preferences-user' htmlFor='user-name'>
             Current Preferences
           </label>
-          <div className="frequency-tweet-posting">
+          <div className='frequency-tweet-posting'>
             <p>Daily posting frequency:</p>
             {user.frequencyTweetPosting}
           </div>
-          <div className="selected-hours">
+          <div className='selected-hours'>
             <p>Posting hours:</p>
             {user.postingHours.map((hour: number) => (
               <p key={hour}>{hour < 10 ? `0${hour}:00 h` : `${hour}:00 h`}</p>
             ))}
           </div>
         </div>
-        <div className="connection-to-twitter">
-          <div className="connection-status">
+        <div className='connection-to-twitter'>
+          <div className='connection-status'>
             <p>Connection to Twitter:</p>
             {user.twitterToken !== null ? 'Connected!' : 'Not connected'}
           </div>
-          <button onClick={handleClick} className="connect-btn">
+          <button onClick={handleClick} className='connect-btn'>
             Connect to Twitter
           </button>
         </div>
