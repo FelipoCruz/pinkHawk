25dcf77ca4705dd269af55e769c398003667f006
Phil Cruz <mfel.cruz@gmail.com>
2023-02-11 19:15:50 +0100

    uncommenting middleware auth

--- server/src/middleware/auth-protect.ts
+++ server/src/middleware/auth-protect.ts
@@ -9,17 +9,17 @@ export const authProtect = (
   next: NextFunction
 ) => {
   try {
-    // const { token } = req.cookies;
-    // console.log('token line', token)
-    // if (!token) {
-    //   console.log('No token');
-    //   res.status(400).json({ error: 'token not found' })
-    // }
-    // const decoded = jwt.verify(token, SECRET_KEY);
-    // if (!decoded) {
-    //   console.log('No decoded');
-    //   res.status(400).json({ error: 'token not valid' })
-    // }
+    const { token } = req.cookies;
+    console.log('token line', token)
+    if (!token) {
+      console.log('No token');
+      res.status(400).json({ error: 'token not found' })
+    }
+    const decoded = jwt.verify(token, SECRET_KEY);
+    if (!decoded) {
+      console.log('No decoded');
+      res.status(400).json({ error: 'token not valid' })
+    }
     next();
   } catch (error) {
     console.log('unexpected error in authProtect', error);
