7cc592a00cfd741c40d2d8a75dbe45265249f0ee
IsNotSally <verena0915@gmail.com>
2023-02-17 19:16:09 +0100

    get followers info apiservice

--- client/src/app/components/growthmenu/GrowthMenu.tsx
+++ client/src/app/components/growthmenu/GrowthMenu.tsx
@@ -1,9 +1,57 @@
-import React from 'react';
+import { stat } from 'fs';
+import React, { useEffect, useState } from 'react';
+import { Doughnut } from 'react-chartjs-2';
+import { getFollowers } from '../../../services/api.service';
+import { useAppSelector } from '../../hooks/hooks';
+
 
 const GrowthMenu = () => {
+
+  const user = useAppSelector(state => state.user);
+  const [followers, setFollowers] = useState<number[]>([]);
+
+  //define a function to get the followers data of the user
+  useEffect(() => {
+    getFollowers().then((data) => {
+      setFollowers(data);
+    });
+  }, [user]);
+
+  const data = {
+    datasets: [
+      {
+        data: followers,
+        backgroundColor: [
+          '#FF6384',
+          '#36A2EB',
+          '#FFCE56',
+          '#8e5ea2',
+          '#3cba9f',
+          '#e8c3b9',
+          '#c45850'
+        ],
+        hoverBackgroundColor: [
+          '#FF6384',
+          '#36A2EB',
+          '#FFCE56',
+          '#8e5ea2',
+          '#3cba9f',
+          '#e8c3b9',
+          '#c45850'
+        ]
+      }
+    ],
+  
+    // These options are optional
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+    },
+  };
+
   return (
     <div>
-      <h2>Growth menu</h2>
+      <Doughnut data={data} />
     </div>
   );
 };
